c4cc2e8dd36587746b08673b81ab555299242a5e
Jonatas <jjonatas@outlook.com>
2024-10-02 00:05:52 -0300

    Criacao de Category

---
+++ src/main/java/com/meva/finance/category/demo/CategoryApplication.java
@@ -0,0 +1,13 @@
+package com.meva.finance.category.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryApplication.class, args);
+	}
+
+}
